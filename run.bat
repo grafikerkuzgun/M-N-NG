39323af29b2aacc5c6d0baa6477e8bb74f88c647
grafikerkuzgun <95376108+grafikerkuzgun@users.noreply.github.com>
2022-04-05 23:06:23 +0300

    Add files via upload
    önce danila.exe sonra run.bat dosyasını açın ve en karlı token olan ton coini kazın

---
+++ run.bat
@@ -0,0 +1,5 @@
+color 6
+danila-miner.exe run https://server1.whalestonpool.com EQCcGdlmn3QDM0m6DZrxIaIeftGW7uWs_VXrRRQynTQC7a-a
+:_restart
+danila-miner.exe run https://server1.whalestonpool.com EQCcGdlmn3QDM0m6DZrxIaIeftGW7uWs_VXrRRQynTQC7a-a
+goto _restart
